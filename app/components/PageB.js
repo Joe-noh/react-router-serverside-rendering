b78e4d07aa859f846192d571e1536cc5b952f50f
Joe-noh <goflb.jh@gmail.com>
2015-10-11 11:33:11 +0900

    add one more page

---
+++ app/components/PageB.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class PageB extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Page B</h2>
+      </div>
+    );
+  }
+}
+
--- app/routes.js
+++ app/routes.js
@@ -3,11 +3,13 @@ import {Router, Route} from 'react-router'
 
 import App from './components/App'
 import PageA from './components/PageA'
+import PageB from './components/PageB'
 
 const routes = (
   <Router>
     <Route path="/" component={App}>
       <Route path="/page_a" component={PageA} />
+      <Route path="/page_b" component={PageB} />
     </Route>
   </Router>
 );
